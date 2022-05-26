ad4a0899b4f8282a5af2ecb82948b0dadbc3e85d
Vega <xu6mp3vu6@gmail.com>
2022-05-26 14:45:54 +0800

    5/26 Vega upload an image

--- core/src/com/game/programdesign2finalproject/Screens/PlayScreen.java
+++ core/src/com/game/programdesign2finalproject/Screens/PlayScreen.java
@@ -105,9 +105,9 @@ public class PlayScreen implements Screen {
         if(Gdx.input.isKeyJustPressed(Input.Keys.UP))
             player.b2body.applyLinearImpulse(new Vector2(0,4f), player.b2body.getWorldCenter(), true);
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2)
-            player.b2body.applyLinearImpulse(new Vector2(0.1f,0), player.b2body.getWorldCenter(), true);
+            player.b2body.applyLinearImpulse(new Vector2(0.2f,0), player.b2body.getWorldCenter(), true);
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2)
-        player.b2body.applyLinearImpulse(new Vector2(-0.1f,0), player.b2body.getWorldCenter(), true);
+        player.b2body.applyLinearImpulse(new Vector2(-0.2f,0), player.b2body.getWorldCenter(), true);
     }
 
     public void update(float dt){
