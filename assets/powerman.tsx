e633b7bc0e793550c230b38e75c17a137908c66e
Vega <xu6mp3vu6@gmail.com>
2022-05-28 02:46:54 +0800

    5/28 Vega add soundtrack

--- assets/powerman.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="powerman" tilewidth="160" tileheight="160" spacing="3" margin="3" tilecount="25" columns="5">
- <image source="../../../Pictures/Saved Pictures/powerman.jpg" width="828" height="828"/>
-</tileset>
